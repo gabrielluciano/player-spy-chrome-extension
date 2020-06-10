5e0cbb3875be9c0f3c353e63a69407a8b4b0e991
Gabriel Luciano de Souza <66069864+gabrielluciano@users.noreply.github.com>
2020-06-10 16:32:48 -0300

    Update content.js
    Fixed undefined errors on tabela1 and tabela2

--- playerspy/content.js
+++ playerspy/content.js
@@ -9,74 +9,76 @@ let linkBlue = ""
 let linkRed = ""
 
 const interval = setInterval(() => {
-    let tabela1 = document.getElementsByClassName('bfy-table')[0]
-    let tabela2 = document.getElementsByClassName('bfy-table')[1]
+    if (document.getElementsByClassName('bfy-table').length == 2) {
+        let tabela1 = document.getElementsByClassName('bfy-table')[0]
+        let tabela2 = document.getElementsByClassName('bfy-table')[1]
 
-    if (tabela1 && tabela2) {
-        let playersBlue = tabela1.querySelectorAll('div#match-extras-content td.player-in-game-name')
-        let playersRed = tabela2.querySelectorAll('div#match-extras-content td.player-in-game-name')
-        let nome1 = ''
-        if (playersBlue != undefined && playersRed != undefined) {
-            if ((firstplayerBlue != playersBlue[0].textContent) || (firstplayerRed != playersRed[0].textContent)) {
-                firstplayerBlue = playersBlue[0].textContent
-                firstplayerRed = playersRed[0].textContent
+        if (tabela1 && tabela2) {
+            let playersBlue = tabela1.querySelectorAll('div#match-extras-content td.player-in-game-name')
+            let playersRed = tabela2.querySelectorAll('div#match-extras-content td.player-in-game-name')
+            let nome1 = ''
+            if (playersBlue != undefined && playersRed != undefined) {
+                if ((firstplayerBlue != playersBlue[0].textContent) || (firstplayerRed != playersRed[0].textContent)) {
+                    firstplayerBlue = playersBlue[0].textContent
+                    firstplayerRed = playersRed[0].textContent
 
-                nicksBlue = []
-                nicksRed = []
+                    nicksBlue = []
+                    nicksRed = []
 
-                for (let j = 0; j < playersBlue.length; j++) {
+                    for (let j = 0; j < playersBlue.length; j++) {
 
-                    nome1 = playersBlue[j].textContent
+                        nome1 = playersBlue[j].textContent
 
-                    for (let i = 0; i < nome1.length; i++) {
-                        if (/\w|á|à|â|ã|é|è|ê|í|ï|ó|ô|õ|ö|ú|ç|ñ/gi.test(nome1[i])) {
-                            nome2 += nome1[i]
+                        for (let i = 0; i < nome1.length; i++) {
+                            if (/\w|á|à|â|ã|é|è|ê|í|ï|ó|ô|õ|ö|ú|ç|ñ/gi.test(nome1[i])) {
+                                nome2 += nome1[i]
+                            }
                         }
+
+                        nicksBlue.push(nome2.replace(/\C$/, ''))
+                        nome2 = ''
                     }
 
-                    nicksBlue.push(nome2.replace(/\C$/, ''))
-                    nome2 = ''
-                }
+                    for (let j = 0; j < playersRed.length; j++) {
 
-                for (let j = 0; j < playersRed.length; j++) {
+                        nome1 = playersRed[j].textContent
 
-                    nome1 = playersRed[j].textContent
-
-                    for (let i = 0; i < nome1.length; i++) {
-                        if (/\w|á|à|â|ã|é|è|ê|í|ï|ó|ô|õ|ö|ú|ç|ñ/gi.test(nome1[i])) {
-                            nome2 += nome1[i]
+                        for (let i = 0; i < nome1.length; i++) {
+                            if (/\w|á|à|â|ã|é|è|ê|í|ï|ó|ô|õ|ö|ú|ç|ñ/gi.test(nome1[i])) {
+                                nome2 += nome1[i]
+                            }
                         }
+
+                        nicksRed.push(nome2.replace(/\C$/, ''))
+                        nome2 = ''
                     }
 
-                    nicksRed.push(nome2.replace(/\C$/, ''))
-                    nome2 = ''
+                    blueButton.innerHTML = "Ver no OP.GG"
+                    redButton.innerHTML = "Ver no OP.GG"
+
+                    blueButton.setAttribute("id", "btnBlue")
+                    redButton.setAttribute("id", "btnRed")
+
+                    document.querySelector("div.card").appendChild(blueButton)
+                    document.querySelector("div.card").appendChild(redButton)
+
+                    linkBlue = `https://br.op.gg/multi/query=${nicksBlue[0]}`
+                    for (let pos = 1; pos < nicksBlue.length; pos++) {
+                        linkBlue += `%2C${nicksBlue[pos]}`
+                    }
+
+                    linkRed = `https://br.op.gg/multi/query=${nicksRed[0]}`
+                    for (let pos = 1; pos < nicksRed.length; pos++) {
+                        linkRed += `%2C${nicksRed[pos]}`
+                    }
+
                 }
 
-                blueButton.innerHTML = "Ver no OP.GG"
-                redButton.innerHTML = "Ver no OP.GG"
+                blueButton.addEventListener("click", clickBlue)
 
-                blueButton.setAttribute("id", "btnBlue")
-                redButton.setAttribute("id", "btnRed")
-
-                document.querySelector("div.card").appendChild(blueButton)
-                document.querySelector("div.card").appendChild(redButton)
-
-                linkBlue = `https://br.op.gg/multi/query=${nicksBlue[0]}`
-                for (let pos = 1; pos < nicksBlue.length; pos++) {
-                    linkBlue += `%2C${nicksBlue[pos]}`
-                }
-
-                linkRed = `https://br.op.gg/multi/query=${nicksRed[0]}`
-                for (let pos = 1; pos < nicksRed.length; pos++) {
-                    linkRed += `%2C${nicksRed[pos]}`
-                }
+                redButton.addEventListener("click", clickRed)
 
             }
-
-            blueButton.addEventListener("click", clickBlue)
-
-            redButton.addEventListener("click", clickRed)
-
         }
     }
 
